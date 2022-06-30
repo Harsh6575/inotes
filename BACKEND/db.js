c02661db4360bd4f0fe0a0be9ba278e6c4322556
Harsh6575 <harsh.190410107146@gmail.com>
2022-06-30 15:52:17 +0530

    c2

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotes";
+// const mongoURI = "mongodb+srv://HPPATEL:aBkzs1Utr1lxi80C@inotes.3x5tp.mongodb.net/test";   
+
+const mongoURI = 'mongodb://localhost:27017/inotes';
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
